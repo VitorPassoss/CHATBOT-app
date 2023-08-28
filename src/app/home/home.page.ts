f80c82e3d25a776b7ca85da87972f1ff25060662
vitor passos <vitorpassosbrit@gmail.com>
2023-08-27 21:58:11 -0300

    updtate

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  @ViewChild('content', { static: false }) content!: IonContent;
 
   constructor(private cdRef: ChangeDetectorRef) {}
 
@@ -14,11 +17,13 @@ export class HomePage {
   capchas: any[] = [
     {
       "imagem_path": "../assets/capts/226md.png",
-      "resposta": "226md"
+      "resposta": "226md",
+      "valor": 1560.00
     },
     {
       "imagem_path": "../assets/capts/2356g.png",
-      "resposta": "2356g"
+      "resposta": "2356g",
+      "valor": 250.50
     },
     {
       "imagem_path": "../assets/capts/2356g.png",
@@ -75,57 +80,114 @@ export class HomePage {
   insertMessage: any[] = [];
   selectedCaptchas: any[] = [];
   insertQueue: any[] = [];
-
+  typingAnimation = { isTyping: true , showLogo: true}; 
+  amount:number = 0.00
   isModalOpen = false;
-  captchaCount = 0; // nova propriedade para contar o número de captchas processados
-
-
+  captchaCount = 0; 
+  firstMessageCount: number = 0;
+  processedMessagesCount: number = 0;
+  showCard: boolean = false;
+  currentCaptchaIndex: number = 0; 
+  processQuant:number = 0
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
+    this.insertMessage = []
     this.initProcess();
+    this.insertMessage.push(this.typingAnimation);
+    setTimeout(()=>{
+      this.insertMessage.pop()
+    }, 2000)
+
   }
 
-  getRandomCapcha(availableCaptchas: any[]): any {
-    const index = Math.floor(Math.random() * availableCaptchas.length);
-    const selectedCaptcha = availableCaptchas[index];
-    availableCaptchas.splice(index, 1);
-    return selectedCaptcha;
+  ngAfterViewInit() {
+    this.scroll();
+}
+
+scroll() {
+  this.content.scrollToBottom(300); // 300ms animation speed
+}
+  
+getRandomCapcha(availableCaptchas: any[]): {captcha: any, index: number} {
+  const index = Math.floor(Math.random() * availableCaptchas.length);
+  const selectedCaptcha = availableCaptchas[index];
+  availableCaptchas.splice(index, 1);
+  return { captcha: selectedCaptcha, index: index };
+}
+initProcess() {
+  if (this.captchaCount >= 4) {
+    this.processQueue();
+    return;
   }
 
-  initProcess() {
-    if (this.captchaCount >= 2) {
-        this.processQueue(); 
-        return; 
-    }
+  let availableCaptchas = [...this.capchas];
+  let { captcha, index } = this.getRandomCapcha(availableCaptchas);  // Use a desestruturação para pegar o captcha e o índice
+  this.currentCaptchaIndex = index;
+  this.selectedCaptchas.push(captcha);
 
-    let availableCaptchas = [...this.capchas];
-    let captcha = this.getRandomCapcha(availableCaptchas);
-    this.selectedCaptchas.push(captcha);
-    this.insertQueue.push(captcha); 
-    this.insertQueue.push({ texto: `Software: ${captcha.resposta}`, showLogo: false });
+  this.insertQueue.push(this.typingAnimation);
+  this.insertQueue.push({imagem_path:captcha.imagem_path, showLogo: true})
+  this.insertQueue.push({ texto: `Software: ${captcha.resposta}`, showLogo: true });
+  this.insertQueue.push({ texto: `Parabéns! Seu saldo agora é R$${captcha.valor}`, showLogo: true , valor:captcha.valor});
+  this.captchaCount++;
 
-    if (captcha.valor) {
-        this.insertQueue.push({ texto: `Parabéns! Seu saldo agora é R$${captcha.valor}`, showLogo: true });
-    } else {
-        this.insertQueue[this.insertQueue.length - 1].showLogo = true;
-    }
-
-    this.captchaCount++;
-
-    setTimeout(() => {
-        this.initProcess();
-    }, 1000);
-  }
+  setTimeout(() => {
+    this.initProcess();
+  }, 500);
+}
 
   processQueue() {
+    this.processQuant++
     if (this.insertQueue.length > 0) {
-      this.insertMessage.push(this.insertQueue.shift());
-      setTimeout(() => {
-        this.processQueue();
-      }, 2000);
-    }
+        
 
-    console.log(this.insertMessage);
-    this.cdRef.detectChanges();
-  }
+        const nextMessage = this.insertQueue.shift();
+
+        if (nextMessage.isTyping) {
+            this.insertMessage.push(nextMessage);
+            
+            
+            setTimeout(() => {
+             
+                this.processQueue();
+            }, 2000);  
+        } else {
+            if (this.processedMessagesCount % 3 === 0) {
+                this.insertMessage.pop();
+            }
+            
+            this.insertMessage.push(nextMessage);
+            
+            if(nextMessage.valor){
+                this.amount = this.amount + nextMessage.valor;
+            }
+            
+            setTimeout(() => {
+                let currentMsg = this.insertMessage[this.insertMessage.length - 1]
+                if(currentMsg.valor){
+                  currentMsg.showLogo = true
+                }else{
+                  currentMsg.showLogo = false
+                }
+                this.processQueue();
+            }, 1500);
+            
+            this.processedMessagesCount++;
+        }
+    } else {
+        this.showCard = true;
+        this.processedMessagesCount = 0;
+    }
+}
+
+
+continueProcess() {
+    this.insertQueue = [];
+    this.captchaCount = 0
+    this.showCard = false
+    this.initProcess();
+}
+
+
+  
 }
